8bb60df6077d073456070ccac89c6310b60e0546
Luis Rodrigo Hernandez Hernandez <rodrigohh24@gmail.com>
2020-07-05 01:30:27 -0600

    Level 4: Hash with salty

--- app.js
+++ app.js
@@ -4,11 +4,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require("mongoose");
-const md5 = require("md5")
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
-
-console.log(process.env.API_KEY);
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
@@ -16,7 +15,7 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-mongoose.connect("mongodb://localhost:27017/userDB", {userNewUrlParser: true});
+mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
 
 
 
@@ -49,9 +48,13 @@ app.get("/register", function(req, res){
 
 
 app.post("/register", function(req,res){
+  const hash = bcrypt.hashSync(req.body.password, saltRounds, function (err, hash){
+
+  });
+
   const newUser = new User({
     email: req.body.username, //This come from "register.ejs" with name=username
-    password: md5(req.body.password) //this come from "register.ejs" with name=password
+    password: hash //this come from "register.ejs" with name=password
   }); //md5 to convert to in ireversible hash
   newUser.save(function(err){
     if (err){
@@ -65,16 +68,18 @@ app.post("/register", function(req,res){
 
 app.post("/login", function (req, res){
   const username = req.body.username;
-  const password = md5(req.body.password);
+  const password = req.body.password;
 
   User.findOne({email: username}, function(err, foundUser){
     if(err){
       console.log(err);
-    }else {
+    } else {
       if(foundUser){
-        if(foundUser.password === password){
-          res.render("secrets");
-        }
+         bcrypt.compare(password, foundUser.password, function(req, result){
+          if (result = true){
+              res.render("secrets");
+          }
+        });
       }
     }
   });
