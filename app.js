5256b06e6249c8befef9c75c7b3775cf093363ff
Luis Rodrigo Hernandez Hernandez <rodrigohh24@gmail.com>
2020-07-19 00:55:17 -0600

    Secutiry 6: authentication with google

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const mongoose = require("mongoose");
 const session = require('express-session');
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
 
 const app = express();
 
@@ -31,26 +33,58 @@ mongoose.set('useCreateIndex', true);
 
 const userSchema = new mongoose.Schema ( { //We added the right part from equal signial for requirements of mongoose encrypt
   email: String,
-  password: String
+  password: String,
+  googleId: String
 });
 
 // const secret = "Thisisourlittlesecret."; //if someone have access to this const, will be able to decrypt your bynary code
 
 userSchema.plugin(passportLocalMongoose); // this plugin is require to set up the configuration of the cookie in the DB
-
+userSchema.plugin(findOrCreate); //FindORCrerate plugin
 const User = new mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());  //Those three lines come exactly like the documentation
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
 
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
 
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets"
+  },
+  function(accessToken, refreshToken, profile, cb) { // Google authentication
+    console.log(profile);
+
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 
 app.get("/", function(req, res){
   res.render("home");
 });
+
+
+app.get("/auth/google",
+  passport.authenticate('google', { scope: ["profile"] } )
+); //bring our pop-auth
+
+app.get("/auth/google/secrets",
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    //successful authentication redirect to the secrets page
+    res.redirect('/secrets');
+  });
 
 app.get("/login", function(req, res){
   res.render("login");
@@ -88,21 +122,8 @@ app.post("/register", function(req,res){
     });
   });
 
-//   const newUser = new User({
-//     email: req.body.username, //This come from "register.ejs" with name=username
-//     password: hash //this come from "register.ejs" with name=password
-//   }); //md5 to convert to in ireversible hash
-//   newUser.save(function(err){
-//     if (err){
-//       console.log(err);
-//     }else {
-//       res.render("secrets");
-//     }
-//   });
-// });
 
-
-app.post("/login", function (req, res){
+app.post("/login", function(req, res){
   const user = new User ({
     username: req.body.username,
     password: req.body.password
